8bef4b96ff89a558cc92b3829281982e607b67cb
Rad Cirskis <nad2000@gmail.com>
2017-08-12 23:20:32 +1200

    working example with V3

--- cmd/root.go
+++ cmd/root.go
@@ -17,13 +17,92 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+func createAddress(raw string) *mail.Email {
+	if raw == "" {
+		log.Fatal("Missing email adderess.")
+	}
+	parts := strings.Split(raw, " <")
+	if len(parts) == 0 || parts[0] == "" {
+		log.Fatal("Email address is incorrect:", raw)
+	}
+	for i, p := range parts {
+		parts[i] = strings.Trim(p, " ><")
+	}
+	if len(parts) < 2 {
+		return mail.NewEmail(parts[0], parts[0])
+	}
+	return mail.NewEmail(parts[0], parts[1])
+}
+
+// Command execution
+func send(cmd *cobra.Command, args []string) {
+	debugCmd(cmd)
+	fmt.Println("*** send called with:", len(args))
+
+	from := createAddress(flagString(cmd, "from"))
+	subject := flagString(cmd, "subject")
+	if subject == "" {
+		log.Fatal("The subject is required. You can get around this requirement if you use a template with a subject defined or if every personalization has a subject defined.")
+	}
+	toAddressesRaw := flagStringArray(cmd, "to")
+
+	if len(toAddressesRaw) == 0 {
+		log.Fatal("At lease one recepient should be present. Please -t or --to flag to specify a recepient.")
+	}
+	toAddresses := make([]*mail.Email, len(toAddressesRaw))
+	for i, toRaw := range toAddressesRaw {
+		toAddresses[i] = createAddress(toRaw)
+	}
+
+	to := toAddresses[0]
+	var plainTextContent string
+	if len(args) > 0 {
+		plainTextContent = args[0]
+	} else {
+		plainTextContent = "and easy to do anywhere, even with Go"
+	}
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	apiKey := flagString(cmd, "key")
+	if apiKey == "" {
+		apiKey = os.Getenv("SENDGRID_API_KEY")
+		if apiKey == "" {
+			log.Fatal("SendGrid API Key is missing...")
+		}
+	}
+	client := sendgrid.NewSendClient(apiKey)
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Error(err)
+	} else {
+		if verbose || debug {
+			log.Info("Status Code:", response.StatusCode)
+			log.Info("Response Body:", response.Body)
+			log.Info("Response Headers:")
+			for k, v := range response.Headers {
+				log.Infof("%s: %v", k, v)
+			}
+		}
+	}
+}
+
+var (
+	cfgFile string
+	debug   bool
+	verbose bool
+)
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -33,7 +112,7 @@ var RootCmd = &cobra.Command{
 and template parameter substitution.
 
 to quickly create a Cobra application.`,
-	//Run: func(cmd *cobra.Command, args []string) {},
+	Run: send,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -53,9 +132,16 @@ func init() {
 	// will be global for your application.
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sendgrid-cli.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.PersistentFlags().BoolP("debug", "d", false, "Show full stack trace on error.")
+	RootCmd.PersistentFlags().BoolP("verbose", "V", false, "Show more verbose details.")
+	RootCmd.PersistentFlags().BoolP("plain", "p", false, "Print result as plain text (where applicable).")
+	RootCmd.PersistentFlags().BoolP("json", "j", false, "Print result as JSON (where applicable).")
+	RootCmd.PersistentFlags().StringP("key", "k", "", "SendGrid API Key.")
+	RootCmd.PersistentFlags().StringP("from", "f", "", "from address")
+	RootCmd.PersistentFlags().StringArrayP("to", "t", []string{}, "to address")
+	RootCmd.PersistentFlags().StringArray("cc", []string{}, "to address")
+	RootCmd.PersistentFlags().StringP("subject", "s", "", "email subject")
+	RootCmd.PersistentFlags().StringP("body", "b", "", "HTML body file name")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -81,5 +167,54 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func flagString(cmd *cobra.Command, name string) string {
+	return cmd.Flag(name).Value.String()
+}
+
+func flagStringSlice(cmd *cobra.Command, name string) (val []string) {
+	val, err := cmd.Flags().GetStringSlice(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func flagStringArray(cmd *cobra.Command, name string) (val []string) {
+	val, err := cmd.Flags().GetStringArray(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func flagBool(cmd *cobra.Command, name string) (val bool) {
+	val, err := cmd.Flags().GetBool(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func flagInt(cmd *cobra.Command, name string) (val int) {
+	val, err := cmd.Flags().GetInt(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func debugCmd(cmd *cobra.Command) {
+	debug = flagBool(cmd, "debug")
+	verbose = flagBool(cmd, "verbose")
+
+	if debug {
+		log.SetLevel(log.DebugLevel)
+		title := fmt.Sprintf("Command %q called with flags:", cmd.Name())
+		fmt.Println(title)
+		fmt.Println(strings.Repeat("=", len(title)))
+		cmd.DebugFlags()
 	}
 }
